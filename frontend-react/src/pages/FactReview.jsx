c08a60a923fbc56744a33c39f4c8ed660d8c98dd
im-pearl <im.pearl.im@gmail.com>
2025-11-18 15:24:27 +0900

    의뢰서 데이터 변경

--- frontend-react/src/pages/FactReview.jsx
+++ frontend-react/src/pages/FactReview.jsx
@@ -30,72 +30,139 @@ function FactReview() {
 
   const [qaItems, setQaItems] = useState([
     {
-      question: '의뢰인이 청구하고자 하는 금액은 얼마인가요?',
-      answer: '1억 원',
+      question: '의뢰인은 성범죄 가해자인가요, 피해자인가요?',
+      answer: '피해자',
       highlights: [],
     },
     {
-      question: '문제가 발생한 연도는 언제인가요?',
-      answer: '2025',
+      question: '다음 중 어떤 범죄를 당하였나요?',
+      answer: '강제추행(성추행)',
       highlights: [],
     },
     {
-      question: '의뢰인이 겪고 있는 상황은 무엇인가요?',
-      answer: '건축사사무소를 운영하며, 설계계약서 없이 진행된 건축허가 후 잔금 미지급 문제가 발생했습니다.',
+      question: '의뢰인이 겪은 상황은 다음 중 어디에 해당하나요?',
+      answer: '가해자가 폭행, 협박, 약물을 이용하여 의뢰인을 반항할 수 없게 하여 추행함',
       highlights: [],
     },
     {
-      question: '상대방의 입장은 무엇인가요?',
-      answer: '잔금 지급 의사 없음',
+      question: '가해자는 구체적으로 어떤 행위를 하였나요?',
+      answer: '강제로 성기, 가슴, 엉덩이 등 신체부위를 만지는 행위, 강제로 포옹, 백허그 등 껴안는 행위, 강제로 입 맞추는 행위',
+      highlights: [],
+    },
+    {
+      question: '강제추행 행위가 일어난 장소는 어디인가요?',
+      answer: '모텔, 사우나, 목욕탕 등 숙박업소',
+      highlights: [],
+    },
+    {
+      question: '의뢰인의 나이가 어떻게 되나요?',
+      answer: '만 19세 이상 (성인)',
+      highlights: [],
+    },
+    {
+      question: '의뢰인은 강제추행으로 인해 상해를 입었나요?',
+      answer: '아니오',
+      highlights: [],
+    },
+    {
+      question: '의뢰인과 가해자의 관계는 어떻게 되나요?',
+      answer: '친구, 선후배, 동료 등 지인관계',
+      highlights: [],
+    },
+    {
+      question: '의뢰인이 확보한 증거자료는 무엇이 있나요?',
+      answer: '가해자가 범행을 인정하는 취지의 대화 내용 (녹음파일, 메시지)',
+      highlights: [],
+    },
+    {
+      question: '의뢰인은 가해자를 고소하였나요?',
+      answer: '고소 후 구약식 판결을 받은 상태',
+      highlights: [],
+    },
+    {
+      question: '가해자는 어떤 입장인가요?',
+      answer: '사실을 모두 인정하고 있음',
+      highlights: [],
+    },
+    {
+      question: '의뢰인은 어떤 입장인가요?',
+      answer: '금전적으로 손해배상까지 받고 싶어요',
+      highlights: [],
+    },
+    {
+      question: '가해자에게 전과가 있나요?',
+      answer: '다른 종류의 전과 있음',
+      highlights: [],
+    },
+    {
+      question: '의뢰인 연령대가 어떻게 되나요?',
+      answer: '20대',
       highlights: [],
     },
     {
       question: '변호사님에게 어떤 도움을 받고 싶으신가요?',
-      answer: '소송 진행 및 분쟁/합의 지원',
+      answer: '소송 진행',
+      highlights: [],
+    },
+    {
+      question: '의뢰인 거주 지역',
+      answer: '서울 관악구',
       highlights: [],
     },
     {
       question: '사건내용',
-      answer: '설계용역 대금 회수와 관련하여 잔금이 미지급된 문제로 인해 법적 대응이 필요한 상황입니다.',
+      answer: '민사 소송을 통해 제가 정신적으로 받은 모든 피해를 조금이나마 보상받고 싶습니다.',
       highlights: [],
     },
   ]);
 
   const requestBasicInfo = {
     '의뢰서 작성일': '2025년 3월 14일',
-    '사건 분류': '일반 민사',
+    '사건 분류': '성범죄',
   };
 
   const [consultationGroups, setConsultationGroups] = useState({
     '기초 정보': [
-      { label: '의뢰인 닉네임', value: '김건축' },
-      { label: '의뢰인 연락처', value: '010-****-****' },
+      { label: '의뢰인 이름(닉네임)', value: '수민' },
+      { label: '의뢰인 연락처', value: '010-6407-4522' },
       { label: '변호사', value: '박변호' },
       { label: '변호사 연락처', value: '010-****-****' },
     ],
     '상담 내용': [
-      { label: '사건명', value: '건축설계용역 미수금 회수', highlights: [] },
+      { label: '사건명', value: '성폭력(강제추행) 피해에 따른 불법행위 손해배상 청구', highlights: [] },
       {
         label: '사실관계',
         value:
-          '소송의뢰인은 건축설계용역을 수행했으나, 상대방은 잔금 일부만 지급하고 나머지는 지급 거부하고 있습니다.',
+          '2023. 7. 8. 토요일 경 가해자와 송별회 술자리를 가졌는데, 가해자가 피해자의 소장품을 핑계로 숙박업소로 유인한 후, 강제적으로 바지 안에 손을 넣는 등의 중한 추행을 한 사건입니다.',
         highlights: [],
       },
-      { label: '상담 결과', value: '소송 진행 및 분쟁, 합의 지원 필요', highlights: [] },
+      { label: '상담 결과', value: '불법행위 손해배상 청구 소송 진행', highlights: [] },
       {
         label: '관련 법리',
         value:
-          '상법 제61조는 상인이 그 영업범위 내에서 타인을 위하여 행위를 한 때에는 이에 대하여 상당한 보수를 청구할 수 있다라고 규정하고 있다...',
+          '불법행위 손해배상 청구(일반 민사법리)에 따른 소송입니다.',
         highlights: [],
       },
-      { label: '증거 판단', value: '업무 수행 증거 및 일부 지급 증거 존재', highlights: [] },
-      { label: '불리 요소', value: '계약서 미작성으로 지급 기준 불명확', highlights: [] },
-      { label: '승소 예상 금액', value: '3억7천만원', highlights: [] },
-      { label: '집행 방안', value: '상대방 보유 부동산 등을 통한 집행 가능', highlights: [] },
+      { 
+        label: '증거 판단', 
+        value: '이미 상대가 약식명령(1,000만원)을 받은 사안으로, 약식명령장을 문서송부촉탁 받아 증거로 제출할 예정입니다. - 1301 형사포탈 문자 메시지 존재 - 사건번호 : 서울서부지검 2023형제27420 호', 
+        highlights: [] 
+      },
+      { 
+        label: '불리 요소', 
+        value: '확정된 형사 사건이 있어, 상대가 부인하거나 부정할 수는 없습니다. 다만, 행위 자체의 수준을 보고 위자료 액수를 줄이겠다는 입장을 밝힐 수는 있습니다.', 
+        highlights: [] 
+      },
+      { label: '승소 예상 금액', value: '2,000만원 전후', highlights: [] },
+      { 
+        label: '집행 방안', 
+        value: '현재 정확히 상대의 재산 내역을 알고 있지는 않으나, 상대가 한참 취업을 하여 일을 할 나이(1993년생)이므로, 급여 압류 등을 진행하여 추심을 지속적으로 해날 수 있을 것으로 보입니다.', 
+        highlights: [] 
+      },
     ],
     '비용 정보': [
-      { label: '착수금', value: '000,000원' },
-      { label: '성공보수', value: '회수 금액의 0%' },
+      { label: '착수금', value: '부가세 포함하여 3,300,000원' },
+      { label: '성공보수', value: '상대로 부터 지급받은 금액의 5%' },
     ],
   });
 
@@ -132,7 +199,7 @@ function FactReview() {
       state: {
         requestData: {
           case_request: {
-            title: '계약서 작성 없이 설계용역을 했는데 터무니없이 낮은 금액을 받았어요',
+            title: '강제추행 피해자입니다. 변호사비 지원 받고 싶습니다.',
             questions: qaItems.map(item => ({
               question: item.question,
               answer: item.answer
@@ -257,7 +324,7 @@ function FactReview() {
                   <Stack gap={4}>
                     <RequestForm
                       basicInfo={requestBasicInfo}
-                      title="계약서 작성 없이 설계용역을 했는데 터무니없이 낮은 금액을 받았어요"
+                      title="강제추행 피해자입니다. 변호사비 지원 받고 싶습니다."
                       qaItems={qaItems}
                       onChange={handleRequestChange}
                       onHighlight={handleRequestHighlight}
